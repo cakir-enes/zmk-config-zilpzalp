98b44a3309e221c33b3d95f2413538b41e93c498
cakir-enes <cakir-enes@outlook.com>
2024-07-22 00:12:57 +0300

    goo

--- config/base_zilpzalp.h
+++ config/base_zilpzalp.h
@@ -47,62 +47,6 @@ combo_##NAME { \
 / {
     combos {
         compatible = "zmk,combos";
-/* usually on base layer */
-        COMBO(q, &kp Q, 1 2)
-        COMBO(z, &kp Z, 18 19)
-        COMBO(v, &kp V, 0 1)
-        COMBO(sch, &macro_sch, 0 2)
-        COMBO(slash, &kp SLASH, 22 23)
-        COMBO(dash, &kp MINUS, 21 22)
-//        COMBO(enter, &kp ENTER, 21 22 23)
-        COMBO(enter, &kp ENTER, 21 23)
-        COMBO(esc, &kp ESC, 19 20)
-/* parentheticals */
-        COMBO(lbrc, &kp LBRC, 0 9)
-        COMBO(lbkt, &kp LBKT, 1 10)
-        COMBO(lpar, &kp LPAR, 2 11)
-        COMBO(lt, &kp LT, 3 12)
-        COMBO(gt, &kp GT, 4 13)
-        COMBO(rpar, &kp RPAR, 5 14)
-        COMBO(rbkt, &kp RBKT, 6 15)
-        COMBO(rbrc, &kp RBRC, 7 16)
-/* caps */
-        COMBO(caps, &caps_word, 11 14)
-        COMBO(capslock, &kp CAPSLOCK, 0 7)
-/* deletion */
-        COMBO(bspc, &kp BSPC, 5 6)
-        COMBO(del, &kp DEL, 6 7)
-        COMBO(delword, &kp LC(BSPC), 5 7)
-/* alternative shifting */
-        COMBO(Sq, &kp LS(Q), 1 2 24)
-        COMBO(Sz, &kp LS(Z), 18 19 24)
-        COMBO(Sv, &kp LS(V), 0 1 24)
-        COMBO(Ssch, &macro_shifted_sch, 0 2 24)
-        COMBO(Sw, &kp LS(W), 0 24)
-        COMBO(Sf, &kp LS(F), 1 24)
-        COMBO(Sp, &kp LS(P), 2 24)
-        COMBO(Sb, &kp LS(B), 3 24)
-        COMBO(Sj, &kp LS(J), 4 27)
-        COMBO(Sl, &kp LS(L), 5 27)
-        COMBO(Su, &kp LS(U), 6 27)
-        COMBO(Sy, &kp LS(Y), 7 27)
-        COMBO(Sr, &kp LS(R), 8 24)
-        COMBO(Ss, &kp LS(S), 9 24)
-        COMBO(St, &kp LS(T), 10 24)
-        COMBO(Sh, &kp LS(H), 11 24)
-        COMBO(Sk, &kp LS(K), 12 24)
-        COMBO(Sx, &kp LS(X), 13 27)
-        COMBO(Sn, &kp LS(N), 14 27)
-        COMBO(Sa, &kp LS(A), 15 27)
-        COMBO(Si, &kp LS(I), 16 27)
-        COMBO(So, &kp LS(O), 17 27)
-        COMBO(Sc, &kp LS(C), 18 24)
-        COMBO(Sg, &kp LS(G), 19 24)
-        COMBO(Sd, &kp LS(D), 20 24)
-        COMBO(Sm, &kp LS(M), 21 27)
-        COMBO(Scomma, &kp SEMI, 22 27)
-        COMBO(Sdot, &kp COLON, 23 27)
-        COMBO(Se, &kp LS(E), 17 26)
     };
 };
 
